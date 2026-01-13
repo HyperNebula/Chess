e4c29811b659a244c68ce438a80bcd41db6031d9
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-01-13 15:21:59 -0700

    Fixed error

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -12,7 +12,7 @@ public class ChessGame {
 
     private TeamColor currentTeam;
 
-    public ChessGame(TeamColor currentTeam) {
+    public ChessGame() {
         this.currentTeam = TeamColor.WHITE;
     }
 
