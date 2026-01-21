8761e37e7ab3bb827f6609a9a1d7b2aa090d5869
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-01-21 14:13:11 -0700

    Moved duplicated directional move code

--- shared/src/main/java/chess/piecemoves/RookMove.java
+++ shared/src/main/java/chess/piecemoves/RookMove.java
@@ -12,32 +12,10 @@ public class RookMove {
 
     public static List<ChessMove> rookMoveSet(ChessBoard board, ChessGame.TeamColor color, ChessPosition myPosition) {
 
-        List<ChessMove> moveList = new ArrayList<>();
-
         int[][] updateArray = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
-        for (int[] updatePos: updateArray) {
+        return DirectionalMove.generateMoveList(updateArray, board, color, myPosition);
 
-            ChessPosition positionCheck = new ChessPosition(myPosition.getRow(), myPosition.getColumn());
-            while (true) {
-                positionCheck.updatePosition(updatePos[0], updatePos[1]);
-
-                if (positionCheck.getRow() > 8 || positionCheck.getColumn() > 8 || positionCheck.getRow() < 1 || positionCheck.getColumn() < 1) {
-                    break;
-                }
-
-                if (board.getPiece(positionCheck) == null) {
-                    moveList.add(new ChessMove(myPosition, new ChessPosition(positionCheck.getRow(), positionCheck.getColumn()), null));
-                } else {
-                    if (board.getPiece(positionCheck).getTeamColor() != color) {
-                        moveList.add(new ChessMove(myPosition, new ChessPosition(positionCheck.getRow(), positionCheck.getColumn()), null));
-                    }
-                    break;
-                }
-            }
-        }
-
-        return moveList;
     }
 
 }
