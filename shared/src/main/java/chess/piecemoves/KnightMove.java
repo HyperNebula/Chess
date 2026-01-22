890b7c3d27ff3bc0598149e75f4b8bee75902251
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-01-22 14:28:16 -0700

    Finished KnightMove.java

---
+++ shared/src/main/java/chess/piecemoves/KnightMove.java
@@ -0,0 +1,21 @@
+package chess.piecemoves;
+
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KnightMove {
+
+    public static List<ChessMove> knightMoveSet(ChessBoard board, ChessGame.TeamColor color, ChessPosition myPosition) {
+
+        int[][] directions = {{2,1}, {2,-1}, {-2,1}, {-2,-1}, {1,2}, {-1,2}, {1,-2}, {-1,-2}};
+
+        return MoveListCalculator.generateMoveListKK(directions, board, color, myPosition);
+
+    }
+
+}
