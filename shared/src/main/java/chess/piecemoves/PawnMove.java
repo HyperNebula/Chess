ea6d6c554dc73600b3fe34dc7149226c4282265e
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-01-22 14:01:17 -0700

    Fixed bugs with PawnMove.java

--- shared/src/main/java/chess/piecemoves/PawnMove.java
+++ shared/src/main/java/chess/piecemoves/PawnMove.java
@@ -7,30 +7,38 @@ import java.util.ArrayList;
 
 public class PawnMove {
 
-    public static List<ChessMove> pawnMoveSet(ChessBoard board, ChessGame.TeamColor color, ChessPosition myPosition, ChessPiece.PieceType promotionPiece)  {
+    public static List<ChessMove> pawnMoveSet(ChessBoard board, ChessGame.TeamColor color, ChessPosition myPosition)  {
 
         List<ChessMove> moveList = new ArrayList<>();
+        ChessPiece.PieceType[] promotionPieces = {ChessPiece.PieceType.ROOK, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.BISHOP};
+
         int rowUpdate;
+        int startRow;
+        int promotionRow;
 
         if (color == ChessGame.TeamColor.WHITE) {
             rowUpdate = 1;
+            startRow = 2;
+            promotionRow = 8;
         } else {
             rowUpdate = -1;
+            startRow = 7;
         }
 
         ChessPosition positionCheck = new ChessPosition(myPosition.getRow() + rowUpdate, myPosition.getColumn());
         if (board.getPiece(positionCheck) == null) {
+            if
             moveList.add(new ChessMove(myPosition, new ChessPosition(positionCheck.getRow(), positionCheck.getColumn()), promotionPiece));
 
             positionCheck.updatePosition(rowUpdate, 0);
-            if (myPosition.getRow() == 2 || board.getPiece(positionCheck) == null) {
+            if (myPosition.getRow() == startRow && board.getPiece(positionCheck) == null) {
                 moveList.add(new ChessMove(myPosition, new ChessPosition(positionCheck.getRow(), positionCheck.getColumn()), promotionPiece));
             }
             positionCheck.updatePosition(-rowUpdate, 0);
 
         }
         positionCheck.updatePosition(0, 1);
-        if (myPosition.positionWithinBoard()) {
+        if (positionCheck.positionWithinBoard()) {
             if (board.getPiece(positionCheck) != null && board.getPiece(positionCheck).getTeamColor() != color) {
                 moveList.add(new ChessMove(myPosition, new ChessPosition(positionCheck.getRow(), positionCheck.getColumn()), promotionPiece));
             }
@@ -38,7 +46,7 @@ public class PawnMove {
 
         positionCheck.updatePosition(0, -2);
 
-        if (myPosition.positionWithinBoard()) {
+        if (positionCheck.positionWithinBoard()) {
             if (board.getPiece(positionCheck) != null && board.getPiece(positionCheck).getTeamColor() != color) {
                 moveList.add(new ChessMove(myPosition, new ChessPosition(positionCheck.getRow(), positionCheck.getColumn()), promotionPiece));
             }
