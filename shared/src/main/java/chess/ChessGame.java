9b1bdd0a3ba61b351ac6c55262ab4f1bfccced75
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-02-05 13:33:14 -0700

    Finished isInCheck method in ChessGame.java

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -155,7 +155,9 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        if
+
+        return attackPositionsSetCalculator(teamColor).contains(gameBoard.getKingPos(teamColor));
+
     }
 
     /**
