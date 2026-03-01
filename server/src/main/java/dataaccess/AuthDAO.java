6f6243f9b1a3eb2c920adf12837343788572781c
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-01 16:25:17 -0700

    Changed AuthDao to use interface

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -5,29 +5,9 @@ import model.DataModel.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AuthDAO {
-
-    private List<AuthData> authDataStorage = new ArrayList<>();
-
-    public AuthData getAuth(String authToken) {
-        for (AuthData auth : authDataStorage) {
-            if (auth.authToken().equals(authToken)) {
-                return auth;
-            }
-        }
-        return null;
-    }
-
-    public void createAuth(String authToken, String username) {
-        authDataStorage.add(new AuthData(authToken, username));
-    }
-
-    public void deleteAuth(AuthData auth) {
-        authDataStorage.remove(auth);
-    }
-
-    public void deleteAll() {
-        authDataStorage = new ArrayList<>();
-    }
-
+public interface AuthDAO {
+    public AuthData getAuth(String authToken);
+    public void createAuth(String authToken, String username);
+    public void deleteAuth(AuthData auth);
+    public void deleteAll();
 }
