6667350a08dfe5e90698263df64896f2bbce15c2
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-01 16:30:21 -0700

    Changed UserDao to use interface

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -2,28 +2,8 @@ package dataaccess;
 
 import model.DataModel.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class UserDAO {
-
-    private List<UserData> userDataStorage = new ArrayList<>();
-
-    public UserData getUser(String username) {
-        for (UserData user : userDataStorage) {
-            if (user.username().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    public void createUser(String username, String password, String email) {
-        userDataStorage.add(new UserData(username, password, email));
-    }
-
-    public void deleteAll() {
-        userDataStorage = new ArrayList<>();
-    }
-
+public interface UserDAO {
+    public UserData getUser(String username);
+    public void createUser(String username, String password, String email);
+    public void deleteAll();
 }
---
+++ server/src/main/java/dataaccess/UserDAOMemory.java
@@ -0,0 +1,29 @@
+package dataaccess;
+
+import model.DataModel.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAOMemory {
+
+    private List<UserData> userDataStorage = new ArrayList<>();
+
+    public UserData getUser(String username) {
+        for (UserData user : userDataStorage) {
+            if (user.username().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void createUser(String username, String password, String email) {
+        userDataStorage.add(new UserData(username, password, email));
+    }
+
+    public void deleteAll() {
+        userDataStorage = new ArrayList<>();
+    }
+
+}
