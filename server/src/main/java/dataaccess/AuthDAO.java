73e0aeb667bf8760909112219b0a2778615468fe
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-01 16:28:48 -0700

    Changed GameDao to use interface

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -2,9 +2,6 @@ package dataaccess;
 
 import model.DataModel.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public interface AuthDAO {
     public AuthData getAuth(String authToken);
     public void createAuth(String authToken, String username);
--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -3,52 +3,14 @@ package dataaccess;
 import chess.ChessGame;
 import model.DataModel.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class GameDAO {
-
-    private List<GameData> gameDataStorage = new ArrayList<>();
-
-    public GameData getGame(int gameID) {
-        for (GameData game : gameDataStorage) {
-            if (game.gameID() == gameID) {
-                return game;
-            }
-        }
-        return null;
-    }
-
-    public List<GameData> listGames() {
-        return gameDataStorage;
-    }
-
-    public void joinGame(GameData game, ChessGame.TeamColor color, String username) {
-        if (color == ChessGame.TeamColor.WHITE) {
-            gameDataStorage.set(gameDataStorage.indexOf(game),
-                    new GameData(game.gameID(), username, game.blackUsername(), game.gameName(), game.game()));
-        } else {
-            gameDataStorage.set(gameDataStorage.indexOf(game),
-                    new GameData(game.gameID(), game.whiteUsername(), username, game.gameName(), game.game()));
-        }
-    }
-
-    public void updateGame(GameData gameData, ChessGame newGame) {
-        gameDataStorage.set(gameDataStorage.indexOf(gameData),
-                new GameData(gameData.gameID(), gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(),
-                        newGame));
-    }
-
-    public void createGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-        gameDataStorage.add(new GameData(gameID, whiteUsername, blackUsername, gameName, game));
-    }
-
-    public void deleteGame(GameData game) {
-        gameDataStorage.remove(game);
-    }
-
-    public void deleteAll() {
-        gameDataStorage = new ArrayList<>();
-    }
-
+public interface GameDAO {
+    public GameData getGame(int gameID);
+    public List<GameData> listGames();
+    public void joinGame(GameData game, ChessGame.TeamColor color, String username);
+    public void updateGame(GameData gameData, ChessGame newGame);
+    public void createGame(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game);
+    public void deleteGame(GameData game);
+    public void deleteAll();
 }
