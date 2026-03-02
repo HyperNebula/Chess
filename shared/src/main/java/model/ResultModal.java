6fe9e5150a45909f8b938942f6bee9aab1f7a471
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-01 19:05:09 -0700

    Added Result Class records

---
+++ shared/src/main/java/model/ResultModal.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.List;
+
+public class ResultModal {
+
+    public record LoginResult(String username, String authToken) {};
+
+    public record RegisterResult(String username, String authToken) {};
+
+    public record LogoutResult(Boolean success) {};
+
+    public record GamesResult(List<DataModel.GameData> gameDataList) {};
+
+    public record CreateGameResult(int gameID) {};
+
+    public record JoinResult(Boolean success) {};
+
+}
