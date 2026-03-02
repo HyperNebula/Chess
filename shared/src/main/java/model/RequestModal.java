da62c3518e50ce0d2388a5bf8660a14bda87162f
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-01 18:50:15 -0700

    Added Request Class records

---
+++ shared/src/main/java/model/RequestModal.java
@@ -0,0 +1,19 @@
+package model;
+
+import chess.ChessGame;
+
+public class RequestModal {
+
+    public record LoginRequest(String username, String password) {};
+
+    public record RegisterRequest(String username, String password, String email) {};
+
+    public record LogoutRequest(String authToken) {};
+
+    public record GamesRequest(String authToken) {};
+
+    public record CreateGameRequest(String authToken, String gameName) {};
+
+    public record JoinRequest(String authToken, ChessGame.TeamColor playerColor, int StringID) {};
+
+}
