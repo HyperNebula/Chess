e67f9d6ed3cf12007589c7d62fc6017a2973f1da
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-01 19:42:04 -0700

    Added UserService.java

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,64 @@
+package service;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.DataModel.*;
+import model.RequestModal.*;
+import model.ResultModal.*;
+
+public class UserService {
+
+    private UserDAO userDB;
+    private AuthDAO authDB;
+
+    public UserService(UserDAO userDB, AuthDAO authDB) {
+        this.userDB = userDB;
+        this.authDB = authDB;
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    public RegisterResult register(RegisterRequest registerRequest) throws AlreadyTakenException {
+        if (userDB.getUser(registerRequest.username()) == null) {
+            throw new AlreadyTakenException("Error: already taken");
+        }
+        userDB.createUser(new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email()));
+
+        String tempAuth = generateToken();
+
+        authDB.createAuth(new AuthData(tempAuth, registerRequest.username()));
+        return new RegisterResult(registerRequest.username(), tempAuth);
+    }
+
+    public LoginResult login(LoginRequest loginRequest) throws UnauthorizedException{
+        UserData tempUserData = userDB.getUser(loginRequest.username());
+
+        if (!Objects.equals(tempUserData.password(), loginRequest.password())) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        String tempAuth = generateToken();
+
+        authDB.createAuth(new AuthData(tempAuth, loginRequest.username()));
+        return new LoginResult(loginRequest.username(), tempAuth);
+
+    }
+
+    public LogoutResult logout(LogoutRequest logoutRequest) throws UnauthorizedException, DataAccessException {
+        AuthData tempAuthData = authDB.getAuth(logoutRequest.authToken());
+
+        if (tempAuthData == null) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        authDB.deleteAuth(tempAuthData);
+
+        return new LogoutResult(true);
+    }
+}
