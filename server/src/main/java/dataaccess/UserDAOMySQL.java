6f477ccea6d1ab0c7ff99f4ba5a379cd811399a2
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-06 21:43:41 -0700

    Added basic UserDAOMySQL class

---
+++ server/src/main/java/dataaccess/UserDAOMySQL.java
@@ -0,0 +1,92 @@
+package dataaccess;
+
+import model.DataModel.*;
+
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class UserDAOMySQL implements UserDAO {
+
+    public UserDAOMySQL() throws DataAccessException {
+        configureDatabase();
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT username, password, email FROM users WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException(String.format("Unable to read data: %s", ex.getMessage()), ex);
+        }
+        return null;
+    }
+
+
+    public void createUser(UserData u) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1, u.username());
+                ps.setString(2, u.password());
+                ps.setString(3, u.email());
+
+                ps.executeQuery();
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException(String.format("Unable to read data: %s", ex.getMessage()), ex);
+        }
+    }
+
+
+    public void deleteAll() throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String statement = "TRUNCATE users";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.executeQuery();
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException(String.format("Unable to read data: %s", ex.getMessage()), ex);
+        }
+    }
+
+    private UserData readUser(ResultSet rs) throws SQLException {
+        String username = rs.getString("username");
+        String password = rs.getString("password");
+        String email = rs.getString("email");
+        return new UserData(username, password, email);
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (PreparedStatement preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()), ex);
+        }
+    }
+}
