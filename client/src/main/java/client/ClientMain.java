93782fafae471190e5d1f371c53cb60f4837fb16
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-03-19 18:28:38 -0600

    Started creating basic commandline interface

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,9 +2,25 @@ package client;
 
 import chess.*;
 
+import java.util.Scanner;
+
 public class ClientMain {
+    private static boolean loggedIn = false;
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        // var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        // System.out.println("♕ 240 Chess Client: " + piece);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("♕ Welcome to CS240 Chess Client. Type 'help' for a list of commands.");
+        while (!loggedIn) {
+            System.out.print("[LOGGED_OUT] >>> ");
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.equals("help")) {
+                System.out.println("register");
+            } else {
+                System.out.println("Not a valid command. Type 'help' for a list of commands.");
+            }
+        }
     }
 }
