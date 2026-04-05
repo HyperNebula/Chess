274cc76e5a650133ca7bf40e780a0158e9bd78b9
HyperNebula <36902158+HyperNebula@users.noreply.github.com>
2026-04-05 12:47:20 -0600

    Added basic methods to send messages

--- client/src/main/java/client/WebSocketClient.java
+++ client/src/main/java/client/WebSocketClient.java
@@ -8,50 +8,73 @@ import websocket.commands.*;
 
 import jakarta.websocket.*;
 
-import java.io.IOException;
+import java.io.Console;
 import java.net.URI;
-import java.net.URISyntaxException;
 
-public class WebSocketClient {
+import static websocket.commands.UserGameCommand.CommandType.*;
+
+public class WebSocketClient extends Endpoint {
 
     Session session;
-    MessageHandler messageHandler;
+    ServerNotificationHandler serverNotificationHandler;
 
-    public WebSocketClient(String url, MessageHandler messageHandler) throws Exception {
+    public WebSocketClient(String url, ServerNotificationHandler serverNotificationHandler) throws Exception {
 
         URI socketURI = new URI(url.replace("http", "ws") + "/ws");
-        this.messageHandler = messageHandler;
+        this.serverNotificationHandler = serverNotificationHandler;
 
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         this.session = container.connectToServer(this, socketURI);
 
-        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
-            @Override
-            public void onMessage(String message) {
-                ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+        this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+            ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
 
-                switch (serverMessage.getServerMessageType()) {
-                    case LOAD_GAME:
-                        LoadGameMessage loadGame = new Gson().fromJson(message, LoadGameMessage.class);
+            switch (serverMessage.getServerMessageType()) {
+                case LOAD_GAME:
+                    LoadGameMessage loadGame = new Gson().fromJson(message, LoadGameMessage.class);
+                    ClientMain.game = loadGame.getGame();
 
-                        messageHandler.notify("Game loaded/updated.");
-                        break;
-                    case NOTIFICATION:
-                        NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
-                        messageHandler.notify("Notification: " + notification.getMessage());
-                        break;
-                    case ERROR:
-                        ErrorMessage error = new Gson().fromJson(message, ErrorMessage.class);
-                        messageHandler.notify("Error: " + error.getMessage());
-                        break;
-                }
+                    System.out.println("Loaded game from " + loadGame.getGame());
+                    BoardPrinter.printBoard();
+
+                    serverNotificationHandler.notify("Game loaded/updated.");
+                    break;
+                case NOTIFICATION:
+                    NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
+                    serverNotificationHandler.notify("Notification: " + notification.getMessage());
+                    break;
+                case ERROR:
+                    ErrorMessage error = new Gson().fromJson(message, ErrorMessage.class);
+                    serverNotificationHandler.notify("Error: " + error.getMessage());
+                    break;
             }
         });
 
     }
 
-    public void makeMove(ChessMove move) {
-        return;
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    public void connect(String authToken, int gameID) throws Exception {
+        UserGameCommand command = new UserGameCommand(CONNECT, authToken, gameID);
+        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+    }
+
+    public void makeMove(String authToken, int gameID, ChessMove move) throws Exception {
+        MakeMoveCommand command = new MakeMoveCommand(MAKE_MOVE, authToken, gameID, move);
+        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+    }
+
+    public void leave(String authToken, int gameID) throws Exception {
+        UserGameCommand command = new UserGameCommand(LEAVE, authToken, gameID);
+        this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        this.session.close();
+    }
+
+    public void resign(String authToken, int gameID) throws Exception {
+        UserGameCommand command = new UserGameCommand(RESIGN, authToken, gameID);
+        this.session.getBasicRemote().sendText(new Gson().toJson(command));
     }
 
 }
